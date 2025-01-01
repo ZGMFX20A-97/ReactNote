6972122cbb89a75f723519eaed5669f3e6e136de
CatinPajamas <catinpajamas1997@gmail.com>
2025-01-01 23:12:47 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+
+import Sidebar from "./components/Sidebar";
 import './App.css';
+import {useEffect, useState} from "react";
+import uuid from "react-uuid"
+import Main from "./components/Main";
+
+
 
 function App() {
+    const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes"))||[]);
+    const [activeNote, setActiveNote] = useState(false);
+
+    useEffect(() => {
+        localStorage.setItem("notes",JSON.stringify(notes));
+
+    }, [notes]);
+
+
+    useEffect(() => {
+        setActiveNote(notes[0].id);
+
+    }, []);
+
+
+
+
+    const onAddNote = () => {
+        const newNote = {
+            id: uuid(),
+            title: "New note",
+            content: "",
+            modDate:Date.now(),
+        };
+        setNotes([...notes, newNote]);
+    }
+
+    const onDeleteNote = (id) => {
+        const filterNotes = notes.filter(note => note.id !== id);
+        setNotes(filterNotes);
+    }
+
+    const onEditNote = () => {
+
+    }
+
+    const onUpdateNote = (updatedNote) => {
+        const updatedNotesArray = notes.map((note) => {
+            if(note.id === updatedNote.id){
+                return updatedNote;
+            }else{
+                return note;
+            }
+        });
+        setNotes(updatedNotesArray);
+    }
+
+    const getActiveNote = () => {
+        return notes.find(note => note.id === activeNote);
+    }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <Sidebar onAddNote={onAddNote}
+              notes={notes}
+              onDeleteNote={onDeleteNote}
+              setActiveNote={setActiveNote}
+              activeNote={activeNote}
+
+     />
+       <Main activeNote={getActiveNote()}
+             onUpdateNote={onUpdateNote}
+       />
+   </div>
   );
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Sidebar.css"
+
+const Sidebar = ({onAddNote, onDeleteNote, activeNote, setActiveNote, notes}) => {
+    const sortedNotes = notes.sort((a, b) => b.modDate - a.modDate);
+
+
+    return (
+        <div className="app-side">
+            <div className="app-sidebar-header">
+                <h1>ノート</h1>
+                <button onClick={onAddNote}>追加</button>
+            </div>
+            <div className="app-side-notes">
+                {sortedNotes.map((note) => (
+                    <div className={`app-side-note ${note.id === activeNote && "active"}`}
+                         onClick={() => setActiveNote(note.id)}
+                         key={note.id}
+                    >
+                        <div className="sidebar-note-title">
+
+                            <strong>{note.title}</strong>
+                            <button onClick={() => onDeleteNote(note.id)}>削除</button>
+
+                        </div>
+                        <p>{note.content}</p>
+                        <small>{new Date(note.modDate).toLocaleDateString("ja-JP", {
+                            hour: "2-digit",
+                            minute: "2-digit"
+                        })}</small>
+                    </div>
+                ))}
+
+            </div>
+        </div>
+    );
+};
+
+
+export default Sidebar;
